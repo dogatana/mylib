a31bd8ee9d00577c4b4288e582fed5599f89bdef
Toshihiko Ichida <dogatana@gmail.com>
2021-12-28 13:22:46 +0900

    yaml, json 両対応。インデント指定追加

--- jprint.py
+++ jprint.py
@@ -2,29 +2,56 @@
 prity print for json
 """
 import json
-
-
-def main(file):
-    hash = read_json(file)
-    print(json.dumps(hash, ensure_ascii=False, indent=2))
-
-
-def read_json(file):
-    with open(file, encoding="utf-8") as fp:
-        return json.load(fp)
-
-
-import sys
+import yaml
 import os.path
+import argparse
+
+
+def main(args):
+    ext = os.path.splitext(args.file)[-1].lower()
+    if args.json or ext == ".json":
+        obj = load_json(args.file)
+    elif args.yaml or ext == ".yml":
+        obj = load_yaml(args.file)
+    else:
+        print(args.file, "unexpected file")
+        exit()
+    if obj is not None:
+        print(json.dumps(obj, indent=args.indent, ensure_ascii=False))
+
+
+def load_json(file):
+    try:
+        with open(file, encoding="utf-8") as fp:
+            return json.load(fp)
+    except Exception as e:
+        print("*** json parse error")
+        print(e)
+        return None
+
+
+
+def load_yaml(file):
+    try:
+        with open(file, "rb") as fp:
+            return yaml.safe_load(fp)
+    except Exception as e:
+        print("*** yaml parse error")
+        print(e)
+        return None
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="jprint - pretty print for json and yaml"
+    )
+    parser.add_argument("-y", "--yaml", help="force yaml", action="store_true")
+    parser.add_argument("-j", "--json", help="force json", action="store_true")
+    parser.add_argument("-i", "--indent", help="indent", type=int, default=4)
+    parser.add_argument("file", help="file to print")
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("usage: jprint file")
-        exit()
-
-    file = sys.argv[1]
-    if not os.path.exists(file):
-        print(file, "not found")
-        exit()
-
-    main(file)
+    args = parse_arguments()
+    main(args)
