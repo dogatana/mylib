e69585fc2df8c9e1b2ed3aa43c1443c3cbf999a3
Toshihiko Ichida <dogatana@gmail.com>
2022-02-16 09:34:31 +0900

    ファイル一覧Excel作成 追加

---
+++ filelist.py
@@ -0,0 +1,38 @@
+""" 指定フォルダのファイル一覧 Excel ファイルを作成 """
+import sys
+import os
+import os.path
+from collections import namedtuple
+import excel
+
+
+def main(dir):
+    print("# dir", dir)
+    lst = []
+    parse(dir, lst)
+
+    create_book(lst)
+
+
+def parse(root, lst):
+    for name in sorted(os.listdir(root)):
+        path = os.path.join(root, name)
+        if os.path.isdir(path):
+            parse(path, lst)
+        else:
+            lst.append(path)
+
+
+def create_book(filelist):
+    xls = excel.excel()
+    book = xls.Workbooks.Add()
+    sheet = book.Worksheets(1)
+    for row, path in enumerate(filelist, start=1):
+        cell = sheet.Range(f"A{row}")
+        cell.Value = path
+        sheet.Hyperlinks.Add(Anchor=cell, Address=os.path.abspath(path))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
