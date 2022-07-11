c226d3da5d3999c5ecc7c564f4300fc0f89495d3
Toshihiko Ichida <dogatana@gmail.com>
2022-07-11 13:20:17 +0900

    svg 埋込対応

--- embed_image.py
+++ embed_image.py
@@ -1,6 +1,7 @@
 import sys
 import os.path
 import base64
+import re
 
 import lxml.html
 
@@ -10,9 +11,45 @@ def main(in_html, out_html):
         src = img.attrib["src"]
         if src.startswith("http://") or src.startswith("https://"):
             continue
+        if define_type(src) == "svg":
+            insert_svg(img, src)
+            continue
+
         new_src = replace_src(in_html, src)
         img.attrib["src"] = new_src
+        continue
     doc.write(out_html, encoding="utf-8", method="html")
+
+def define_type(src):
+    ext = os.path.splitext(src)[1].lower()
+    if ext == ".png":
+        return "image/png"
+    elif ext == ".jpg" or ext == ".jpeg":
+        return "image/jpeg"
+    elif ext == ".png":
+        return "image/gif"
+    elif ext == ".svg":
+        return "svg"
+    else:
+        raise ValueError("unknown type: "+ src)
+
+def insert_svg(img, file):
+    svg = read_svg_as_element(file)
+
+    del img.attrib["src"]
+    img.attrib.update(svg.attrib)
+
+    for child in svg.getchildren():
+        img.append(child)
+
+    img.tag = "svg"
+
+def read_svg_as_element(file):
+    with open(file, "rb") as fp:
+        text = fp.read()
+    breakpoint()
+    doc = lxml.html.fromstring(text)
+    return doc.xpath("//svg")[0]
 
 def replace_src(file, src):
     dir = os.path.dirname(file)
@@ -25,16 +62,6 @@ def replace_src(file, src):
     print("#", new_src[:60], "...")
     return new_src
 
-def define_type(src):
-    ext = os.path.splitext(src)[1].lower()
-    if ext == ".png":
-        return "image/png"
-    elif ext == ".jpg" or ext == ".jpeg":
-        return "image/jpeg"
-    elif ext == ".png":
-        return "image/gif"
-    else:
-        raise ValueError("unknown type: "+ src)
 
 def encode(img_file):
     img = open(img_file, "rb").read()
