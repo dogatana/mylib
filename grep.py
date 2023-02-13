c065dc82b672f869cd66d47e11297974d8dfbcae
Toshihiko Ichida <dogatana@gmail.com>
2023-02-14 08:37:46 +0900

    grep -s オプションを追加

--- grep.py
+++ grep.py
@@ -5,11 +5,13 @@ import os.path
 import re
 from encodings.aliases import aliases
 
+from miscutil import expand_files
+
 
 def main(args):
-    pattern = re.compile(args.pattern)
-    files = collect_files(args.file)
+    files = expand_files(args.file)
     args.show_name = len(files) > 1
+    pattern = re.compile(args.pattern, flags=args.ignorecase)
 
     for file in files:
         if not os.path.exists(file):
@@ -29,17 +31,24 @@ def grep(file, pattern, option):
     count = 0
     for n, raw_line in enumerate(open(file, encoding=option.encoding), start=1):
         line = raw_line[: len(raw_line) - 1]  # remove \n at the end
-        m = pattern.search(line)
-        if m is None:
+        if option.scan:
+            result = re.findall(pattern, line)
+        else:
+            m = pattern.search(line)
+            result = [] if m is None else [line]
+        if not result:
             continue
+
         if option.count:
             count += 1
             continue
+        header = ""
         if option.show_name:
-            print(f"{file}:", end="")
+            header += f"{file}:"
         if option.number:
-            print(f"{n}:", end="")
-        print(line)
+            header += f"{n}:"
+        for item in result:
+            print(header + item)
     if option.count:
         print(f"{file}:{count}")
 
@@ -69,6 +78,17 @@ def parse_arguments():
         metavar="ENCODING",
         default="utf-8",
         help="file encoding, default is utf-8",
+    )
+    parser.add_argument(
+        "-i",
+        "--ignorecase",
+        action="store_const",
+        const=re.IGNORECASE,
+        default=0,
+        help="ignore case",
+    )
+    parser.add_argument(
+        "-s", "--scan", action="store_true", default=False, help="scan mathed patterns"
     )
     parser.add_argument("pattern", help="regexp pattern")
     parser.add_argument("file", nargs="+", help="file to be grep-ed")
