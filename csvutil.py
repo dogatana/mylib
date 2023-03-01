9e9876cd5ecff7475f987f422e8fab6c73e7bd89
Toshihiko Ichida <dogatana@gmail.com>
2023-03-02 08:32:39 +0900

    csvuti: 文字列のみ " でクオート

--- csvutil.py
+++ csvutil.py
@@ -2,4 +2,10 @@ def to_csvline(lst):
     def to_column(v):
         return '"' + str(v).replace('"', '""') + '"'
 
+    quoted = []
+    for v in lst:
+        if isinstance(v, str):
+            quoted.append(to_column(v))
+        else:
+            quoted.append(v)
     return ",".join([to_column(v) for v in lst])
