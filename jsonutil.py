69ce31458438023688b8f8ea22c77404e8e39785
Toshihiko Ichida <dogatana@gmail.com>
2023-03-03 14:45:15 +0900

    dump_json も datetime をエンコード可能に default を指定

--- jsonutil.py
+++ jsonutil.py
@@ -7,6 +7,12 @@ def load_json(file, encoding="utf-8"):
         return json.load(fp)
 
 
+def _default_encoder(obj):
+    if isinstance(obj, datetime):
+        return str(obj)
+    raise TypeError(f"{obj} is not serializable")
+
+
 def dump_json(obj, file, encoding="utf-8", ensure_ascii=False, indent=2):
     if indent is None:
         sep = (",", ":")
@@ -14,15 +20,14 @@ def dump_json(obj, file, encoding="utf-8", ensure_ascii=False, indent=2):
         sep = (",", ": ")
     with open(file, "w", encoding=encoding) as fp:
         return json.dump(
-            obj, fp, ensure_ascii=ensure_ascii, indent=indent, separators=sep
+            obj,
+            fp,
+            ensure_ascii=ensure_ascii,
+            indent=indent,
+            separators=sep,
+            default=_default_encoder,
         )
 
 
 def to_json(obj, indent=None):
     return json.dumps(obj, ensure_ascii=False, indent=indent, default=_default_encoder)
-
-
-def _default_encoder(obj):
-    if isinstance(obj, datetime):
-        return str(obj)
-    raise TypeError(f"{obj} is not serializable")
